17fe55af0cc65e721d5d1b79255cfe982493ccf4
naimur-pigeon <naimur@pigeon-soft.com>
2023-01-24 16:18:32 +0600

    payment-method-crud

--- app/Http/Controllers/PaymentMethodController.php
+++ app/Http/Controllers/PaymentMethodController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\PaymentMethod;
 use Illuminate\Http\Request;
+use Yajra\DataTables\Facades\DataTables;
 
 class PaymentMethodController extends Controller
 {
@@ -12,9 +13,33 @@ class PaymentMethodController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    function __construct()
     {
-        //
+        $this->middleware('permission:payment-method.management|payment-method.create|payment-method.edit|payment-method.delete', ['only' => ['index','store']]);
+        $this->middleware('permission:payment-method.create', ['only' => ['create','store']]);
+        $this->middleware('permission:payment-method.edit', ['only' => ['edit','update']]);
+        $this->middleware('permission:payment-method.delete', ['only' => ['destroy']]);
+    }
+    public function index(Request $request)
+    {
+        $data = PaymentMethod::all();
+        if ($request->ajax()) {
+            $data = PaymentMethod::orderBy("id","desc")->get();
+            return  DataTables::of($data)
+                    ->addIndexColumn()
+                        ->addColumn('action', function($row){
+                        $id = $row->id;
+
+                        $edit = route('payment-method.edit',$id);
+                        $delete = route('payment-method.destroy',$id);
+                        $permission = 'payment-method.edit';
+                        $permissiondelete = 'payment-method.delete';
+                        return view('admin.action.action', compact('id','edit','delete','permission','permissiondelete'));
+                    })
+                    ->rawColumns(['action'])
+                    ->make(true);
+        }
+        return view('admin.payment.payment',compact('data'));
     }
 
     /**
@@ -35,7 +60,13 @@ class PaymentMethodController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'method_name' => 'required|string',
+
+
+        ]);
+        $method_name = PaymentMethod::create($request->all());
+         return response()->json($method_name);
     }
 
     /**
@@ -57,7 +88,8 @@ class PaymentMethodController extends Controller
      */
     public function edit(PaymentMethod $paymentMethod)
     {
-        //
+        $method_name =PaymentMethod::find($paymentMethod->id);
+        return response()->json($method_name);
     }
 
     /**
@@ -69,7 +101,15 @@ class PaymentMethodController extends Controller
      */
     public function update(Request $request, PaymentMethod $paymentMethod)
     {
-        //
+        $request->validate([
+            'method_name' => 'required|string',
+
+
+        ]);
+            $input = $request->all();
+
+            $method_name = $paymentMethod->update($input);
+            return response()->json($method_name);
     }
 
     /**
@@ -80,6 +120,7 @@ class PaymentMethodController extends Controller
      */
     public function destroy(PaymentMethod $paymentMethod)
     {
-        //
+        $paymentMethod->delete();
+        return redirect()->route('payment-method.index')->with('success','Delete successfully');
     }
 }
