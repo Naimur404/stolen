c8d7e9fd4bd8716fb63ae08706b293d308893992
Ariful Islam <ariful.bpi2012@gmail.com>
2023-01-26 10:42:06 +0600

    Warehouse and Outlet model added

---
+++ app/Http/Controllers/OutletHasUserController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OutletHasUser;
+use Illuminate\Http\Request;
+
+class OutletHasUserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\OutletHasUser  $outletHasUser
+     * @return \Illuminate\Http\Response
+     */
+    public function show(OutletHasUser $outletHasUser)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\OutletHasUser  $outletHasUser
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(OutletHasUser $outletHasUser)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\OutletHasUser  $outletHasUser
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, OutletHasUser $outletHasUser)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\OutletHasUser  $outletHasUser
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(OutletHasUser $outletHasUser)
+    {
+        //
+    }
+}
---
+++ database/migrations/2023_01_26_042659_create_outlets_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('outlets', function (Blueprint $table) {
+            $table->id();
+            $table->string('outlet_name')->nullable();
+            $table->text('address')->nullable();
+            $table->string('mobile')->nullable();
+            $table->text('details')->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('outlets');
+    }
+};
