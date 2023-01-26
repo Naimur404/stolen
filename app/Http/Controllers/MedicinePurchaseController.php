972faee5905681fa56d77d05297c5799df40bcd1
Ariful Islam <ariful.bpi2012@gmail.com>
2023-01-27 02:52:46 +0600

    Medicine Purchase model added

---
+++ app/Http/Controllers/MedicinePurchaseController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MedicinePurchase;
+use Illuminate\Http\Request;
+
+class MedicinePurchaseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\MedicinePurchase  $medicinePurchase
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MedicinePurchase $medicinePurchase)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\MedicinePurchase  $medicinePurchase
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(MedicinePurchase $medicinePurchase)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\MedicinePurchase  $medicinePurchase
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MedicinePurchase $medicinePurchase)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\MedicinePurchase  $medicinePurchase
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MedicinePurchase $medicinePurchase)
+    {
+        //
+    }
+}
