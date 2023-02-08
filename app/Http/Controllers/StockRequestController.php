d4565933ec6f675b2d6f45e2a103bfaa7e66625b
Ariful Islam <ariful.bpi2012@gmail.com>
2023-02-08 13:48:58 +0600

    Stock Request model added

---
+++ app/Http/Controllers/StockRequestController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\StockRequest;
+use Illuminate\Http\Request;
+
+class StockRequestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\StockRequest  $stockRequest
+     * @return \Illuminate\Http\Response
+     */
+    public function show(StockRequest $stockRequest)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\StockRequest  $stockRequest
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(StockRequest $stockRequest)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\StockRequest  $stockRequest
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, StockRequest $stockRequest)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\StockRequest  $stockRequest
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(StockRequest $stockRequest)
+    {
+        //
+    }
+}
