ce4c19b8f2f5cbe2def40bce94271960517f0e77
Ariful Islam <ariful.bpi2012@gmail.com>
2023-02-20 13:07:41 +0600

    Distribution model updated

---
+++ database/migrations/2023_02_20_070232_add_fields_to_medicine_distribute_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('medicine_distributes', function (Blueprint $table) {
+            $table->boolean('has_sent')->default(false)->after('remarks');
+            $table->boolean('has_received')->default(false)->after('has_sent');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('medicine_distributes', function (Blueprint $table) {
+            $table->dropColumn('has_sent');
+            $table->dropColumn('has_received');
+        });
+    }
+};
