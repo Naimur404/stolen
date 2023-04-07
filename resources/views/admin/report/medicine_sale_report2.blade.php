8a610258669fbca897ce8578023e669741a8d330
Ariful Islam <ariful.bpi2012@gmail.com>
2023-04-07 08:47:06 +0600

    Sales reports issue fixed

--- resources/views/admin/report/medicine_sale_report2.blade.php
+++ resources/views/admin/report/medicine_sale_report2.blade.php
@@ -1,125 +1,112 @@
 @extends('layouts.admin.public_layouts')
 @section('title','Medicine Sales Report')
 @section('main-content')
-<style>
-    .space{
-        line-height: 3px;
-    }
-    .all-content{
+    <style>
+        .space {
+            line-height: 3px;
+        }
+
+        .all-content {
             margin-top: 60px !important;
             min-width: 1000px;
             max-width: 1000px;
             margin: 0 auto;
         }
-    .table tbody tr td{
+
+        .table tbody tr td {
             /* border-top: none; */
             border: 1px solid #070707;
             padding: 2px;
         }
-    .table thead tr th{
+
+        .table thead tr th {
             /* border-top: none; */
             border: 1px solid #070707;
             padding: 2px;
             text-align: center;
         }
-</style>
+    </style>
 
-@php
-    $total_amount = 0;
-@endphp
+    @php
+        $total_amount = 0;
+    @endphp
 
-<div class="row">
-    <div class="col-md-5">
-        <img src="{{asset('uploads/'.$app_setting->logo)}}" alt="" style="max-height: 150px">
+    <div class="row">
+        <div class="col-md-5">
+            <img src="{{asset('uploads/'.$app_setting->logo)}}" alt="" style="max-height: 150px">
+        </div>
+        <div class="col-md-7 text-right">
+            <br>
+            <h4>{{ $title }}</h4>
+            {{-- <p>{{ $business->address }} <br> {{ $business->mobile }}</p> --}}
+        </div>
     </div>
-    <div class="col-md-7 text-right">
-        <br>
-        <h4>{{ $title }}</h4>
-        {{-- <p>{{ $business->address }} <br> {{ $business->mobile }}</p> --}}
-    </div>
-</div>
 
-<div class="row all-content">
-    <p align="center"><b> Medicine Sales Report -
-        @if ($start_date && $end_date !=null )
-           From {{Carbon\Carbon::parse($start_date)->format('d-m-Y')}}
-           To {{Carbon\Carbon::parse($end_date)->format('d-m-Y') }}
-       @endif
-   </b></p>
-    <div class="col-md-12">
-        <table class="table table-hover table-bordered">
-            <thead class="">
+    <div class="row all-content">
+        <p align="center"><b> Medicine Sales Report -
+                @if ($start_date && $end_date !=null )
+                    From {{Carbon\Carbon::parse($start_date)->format('d-m-Y')}}
+                    To {{Carbon\Carbon::parse($end_date)->format('d-m-Y') }}
+                @endif
+            </b></p>
+        <div class="col-md-12">
+            <table class="table table-hover table-bordered">
+                <thead class="">
                 <tr>
                     <th>SL</th>
-
                     <th>Outlet Name</th>
-                    <th>Purchase Date</th>
-                    <th>Payment Method</th>
-                    <th>Purchased By</th>
+                    <th>Sale Date</th>
+                    <th>Method</th>
+                    <th>Sold By</th>
                     <th>Total</th>
                     <th>Pay</th>
                     <th>Due</th>
-                    <th>Sale By</th>
                 </tr>
-            </thead>
-            <tbody>
+                </thead>
+                <tbody>
                 @php
-                $grand_total = 0;
-                $total_due = 0;
-                $total_pay = 0;
+                    $grand_total = 0;
+                    $total_due = 0;
+                    $total_pay = 0;
                 @endphp
-                    @foreach ($productSales as $productPurchase)
+                @foreach ($productSales as $productSale)
                     <tr>
                         <td>{{ $loop->index + 1 }}</td>
-                        @if ($productPurchase->outlet_id == null )
-
-                            <td> N/A </td>
-                        @elseif ($productPurchase->outlet_id != null )
-                            <td>{{ $productPurchase->outlet->outlet_name }}</td>
-
-                        @endif
-
-                        <td>{{ \Carbon\Carbon::parse($productPurchase->purchase_date)->format('d-m-Y') }}
+                        <td>{{ $productSale->outlet->outlet_name ?? '' }}</td>
+                        <td>{{ \Carbon\Carbon::parse($productSale->sale_date)->format('d-m-Y') }}
                         </td>
-                        <td>{{ $productPurchase->payment->method_name }}</td>
-                        @if ($productPurchase->customer_id == null )
+                        <td>{{ $productSale->payment->method_name ?? '' }}</td>
 
-                        <td> Walking Customer </td>
-                    @elseif ($productPurchase->customer_id != null )
-                    <td>{{ $productPurchase->customer->name  }}</td>
 
-                    @endif
+                        <td>{{ $productSale->user->name ?? '' }}</td>
 
-                        <td>{{ $productPurchase->grand_total }}</td>
-                        <td>{{ $productPurchase->paid_amount }}</td>
-                        @if ($productPurchase->due_amount > 0)
-                            <td> {{ $productPurchase->due_amount }} </td>
-                        @else
-                            <td>Paid</td>
-                        @endif
-                        <td>{{ $productPurchase->user->name }}</td>
+
+                        <td>{{ $productSale->grand_total }}</td>
+                        <td>{{ $productSale->paid_amount }}</td>
+                        <td>
+                            {{ $productSale->due_amount > 0 ? $productSale->due_amount : 'Paid' }}
+                        </td>
+
                     </tr>
                     @php
-                    $grand_total = $grand_total + $productPurchase->grand_total;
-                    $total_due = $total_due + $productPurchase->paid_amount;
-                    $total_pay = $total_pay + $productPurchase->due_amount;
+                        $grand_total = $grand_total + $productSale->grand_total;
+                        $total_due = $total_due + $productSale->due_amount;
+                        $total_pay = $total_pay + $productSale->paid_amount;
                     @endphp
                 @endforeach
 
 
-            </tbody>
-        </table>
+                </tbody>
+            </table>
 
-        <p class="text-center">Grand Total {{ $grand_total }} | Total Pay {{ $total_pay }} | Total Due {{ $total_due }}</p>
-        <p class="text-center" style="font-size: 12px">Thank You ❤ Software by Pigeon Soft</p>
+            <p class="text-center">Grand Total {{ $grand_total }} | Total Pay {{ $total_pay }} | Total
+                Due {{ $total_due }}</p>
 
+        </div>
     </div>
-</div>
-@section('custom-js')
-<script>
-    setTimeout(function() { window.print(); }, 1000);
-</script>
+    @section('custom-js')
 
-@endsection
+
+    @endsection
 
 @endsection
