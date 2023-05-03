88a16197e2a8abf10fff1b7e83bf430990254930
naimur-pigeon <naimur@pigeon-soft.com>
2023-05-03 17:24:55 +0600

    feature/sales-report-medicine

--- app/Http/Controllers/ReportController2.php
+++ app/Http/Controllers/ReportController2.php
@@ -19,6 +19,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use App\Models\Category;
+use App\Models\Medicine;
 
 class ReportController2 extends Controller
 {
@@ -270,6 +271,30 @@ class ReportController2 extends Controller
 
         return view('admin.report.sale_report_details', compact('title', 'productSales'));
     }
+
+
+
+    public function sale_report_medicine_submit(Request $request)
+    {
+        $input = $request->all();
+        $start_date = Carbon::parse($input['start_date']);
+        $end_date = Carbon::parse($input['end_date']);
+        $medicine = Medicine::where('id', $request->medicine_id)->orderby('id', 'desc')->first('medicine_name');
+        if (Auth::user()->hasRole(['Super Admin', 'Admin'])) {
+            $productSales = DB::table('outlet_invoice_details')->where('medicine_id', $request->medicine_id)->whereDate('created_at', '>=', $start_date)
+                ->whereDate('created_at', '<=', $end_date)->orderBy('id', 'asc')->get();
+            $title = 'Medicine Name ' . $medicine->medicine_name;
+        } else {
+            $outlet_id = Auth::user()->outlet_id != null ? Auth::user()->outlet_id : outlet::orderby('id', 'desc')->first('id');
+            $productSales = $productSales = DB::table('outlet_invoice_details')->where('outlet_id', $outlet_id)->where('medicine_id', $request->medicine_id)->whereDate('created_at', '>=', $start_date)
+            ->whereDate('created_at', '<=', $end_date)->orderBy('id', 'asc')->get();
+
+
+            $title = 'Medicine Name ' . $medicine->medicine_name;
+        }
+        return view('admin.report.medicine_sale_report_by_medicine', compact('start_date', 'end_date', 'productSales', 'title'));
+    }
+
 
     public function medicine_sale_report_by_user(Request $request)
     {
---
+++ resources/views/admin/report/medicine_sale_report_by_medicine.blade.php
@@ -0,0 +1,114 @@
+@extends('layouts.admin.public_layouts')
+@section('title','Medicine Sales Report')
+@section('main-content')
+<style>
+    .space{
+        line-height: 3px;
+    }
+    .all-content{
+            margin-top: 60px !important;
+            min-width: 1000px;
+            max-width: 1000px;
+            margin: 0 auto;
+        }
+    .table tbody tr td{
+            /* border-top: none; */
+            border: 1px solid #070707;
+            padding: 2px;
+        }
+    .table thead tr th{
+            /* border-top: none; */
+            border: 1px solid #070707;
+            padding: 2px;
+            text-align: center;
+        }
+</style>
+
+@php
+    $total_amount = 0;
+@endphp
+
+<div class="row">
+    <div class="col-md-5">
+        <img src="{{asset('uploads/'.$app_setting->logo)}}" alt="" style="max-height: 150px">
+    </div>
+    <div class="col-md-7 text-right">
+        <br>
+        <h4>{{ $title }}</h4>
+        {{-- <p>{{ $business->address }} <br> {{ $business->mobile }}</p> --}}
+    </div>
+</div>
+
+<div class="row all-content">
+    <p align="center"><b> Medicine Sales Report -
+        @if ($start_date && $end_date !=null )
+           From {{Carbon\Carbon::parse($start_date)->format('d-m-Y')}}
+           To {{Carbon\Carbon::parse($end_date)->format('d-m-Y') }}
+       @endif
+   </b></p>
+    <div class="col-md-12">
+        <table class="table table-hover table-bordered">
+            <thead class="">
+            <tr>
+
+                <th>Sale Date</th>
+                <th>Invoice ID</th>
+                <th>Medicine Name</th>
+                <th>Sale Quantity</th>
+                <th>Rate</th>
+                <th>Total</th>
+
+            </tr>
+            </thead>
+            <tbody>
+            @php
+                $quantity = 0;
+                $discount = 0;
+                $total = 0;
+
+            @endphp
+            @foreach ($productSales as $productSale)
+                <tr>
+                    <td>{{ \Carbon\Carbon::parse($productSale->created_at)->format('d-m-Y') }}
+                    <td>{{ $productSale->outlet_invoice_id }}</td>
+                    <td>{{ $productSale->medicine_name }}
+                    </td>
+
+                    <td>{{ $productSale->quantity ?? '' }}</td>
+                    <td>{{ $productSale->rate }}
+                    </td>
+
+                    <td>{{ $productSale->total_price ?? '' }}</td>
+
+
+
+
+                </tr>
+                @php
+                    $quantity = $quantity + $productSale->quantity;
+                    $discount = $discount + $productSale->discount;
+                    $total = $total + $productSale->total_price;
+
+                @endphp
+            @endforeach
+
+
+            </tbody>
+        </table>
+
+        <p class="text-center">Total {{ $total }} | Discount {{ $discount }} | Quantity
+             {{ $quantity }}</p>
+
+    </div>
+
+
+</div>
+@section('custom-js')
+@if ($app_setting->print == 1)
+<script>
+    setTimeout(function() { window.print(); }, 1000);
+</script>
+@endif
+@endsection
+
+@endsection
