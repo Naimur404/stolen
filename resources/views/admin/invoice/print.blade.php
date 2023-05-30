d177480e5eee011bcf34923f6f9dd6cc38faca8b
Ariful Islam <ariful.bpi2012@gmail.com>
2023-05-31 02:18:45 +0600

    Update copyright and footer credit

--- resources/views/admin/invoice/print.blade.php
+++ resources/views/admin/invoice/print.blade.php
@@ -236,12 +236,13 @@
     <br>
     <div class="row justify-content-center mt-3">
         <img style="margin-left: 87px"
-             src="data:image/png;base64, {!! DNS1D::getBarcodePNG("$outletInvoice->id", 'C39') !!}" alt="barcode"/>
+             src="data:image/png;base64, {!! DNS1D::getBarcodePNG("$outletInvoice->id", 'C39', 2) !!}" alt="barcode"/>
     </div>
 
     <div class="footer">
+        <h4>A Concern of BlueMoon Group</h4>
         <h4>Thank You ❤</h4>
-        <h6>Software by Pigeon Soft</h6>
+{{--        <h6>Software by Pigeon Soft</h6>--}}
     </div>
 
 
--- resources/views/layouts/admin/master.blade.php
+++ resources/views/layouts/admin/master.blade.php
@@ -1,4 +1,16 @@
 <!DOCTYPE html>
+
+<!--
+    ========================================
+    PharmAssist - Pharmacy Management Application
+    Developed by Pigeon Soft
+    Website : www.pigeon-soft.com
+    Mobile  : +8801737569833
+
+    All rights reserved. © Pigeon Soft 2016 - {{ date('Y') }}
+    ========================================
+-->
+
 <html lang="en">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -84,4 +96,16 @@
 </script>
 
 </body>
+
+<!--
+    ========================================
+    PharmAssist - Pharmacy Management Application
+    Developed by Pigeon Soft
+    Website : www.pigeon-soft.com
+    Mobile  : +8801737569833
+
+    All rights reserved. © Pigeon Soft 2016 - {{ date('Y') }}
+    ========================================
+-->
+
 </html>
